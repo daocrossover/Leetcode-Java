d635ced512e33aace69d179c2f9939b36b973898
daocrossover <475970883@qq.com>
2018-11-18 11:23:06 -0800

    11.18

--- src/stack/FlattenNestedListIterator.java
+++ src/stack/FlattenNestedListIterator.java
@@ -18,24 +18,10 @@ Explanation: By calling next repeatedly until hasNext returns false,
              the order of elements returned by next should be: [1,4,6].
  */
 
+import common.NestedInteger;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Stack;
-
-// This is the interface that allows for creating nested lists.
-// You should not implement it, or speculate about its implementation
-interface NestedInteger {
-    // @return true if this NestedInteger holds a single integer, rather than a nested list.
-    public boolean isInteger();
-
-    // @return the single integer that this NestedInteger holds, if it holds a single integer
-    // Return null if this NestedInteger holds a nested list
-    public Integer getInteger();
-
-    // @return the nested list that this NestedInteger holds, if it holds a nested list
-    // Return null if this NestedInteger holds a single integer
-    public List<NestedInteger> getList();
-}
 
 public class FlattenNestedListIterator {
     public class NestedIterator implements Iterator<Integer> {
