e2818fac83ef73cee17ee490a1a4183705323f58
daocrossover <475970883@qq.com>
2019-01-18 01:02:21 -0800

    1.17

---
+++ src/heap/SortCharactersByFrequency.java
@@ -0,0 +1,88 @@
+package heap;
+
+/* 451. Sort Characters By Frequency
+Description:
+Given a string, sort it in decreasing order based on the frequency of characters.
+
+Example 1:
+Input:
+"tree"
+
+Output:
+"eert"
+
+Explanation:
+'e' appears twice while 'r' and 't' both appear once.
+So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+
+Example 2:
+Input:
+"cccaaa"
+
+Output:
+"cccaaa"
+
+Explanation:
+Both 'c' and 'a' appear three times, so "aaaccc" is also a valid answer.
+Note that "cacaca" is incorrect, as the same characters must be together.
+
+Example 3:
+Input:
+"Aabb"
+
+Output:
+"bbAa"
+
+Explanation:
+"bbaA" is also a valid answer, but "Aabb" is incorrect.
+Note that 'A' and 'a' are treated as two different characters.
+ */
+
+import java.util.*;
+
+public class SortCharactersByFrequency {
+    // Solution1: HashMap + PriorityQueue
+    public String frequencySort(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
+        pq.addAll(map.entrySet());
+        StringBuilder sb = new StringBuilder();
+        while (!pq.isEmpty()) {
+            Map.Entry e = pq.poll();
+            for (int i = 0; i < (int)e.getValue(); ++i) {
+                sb.append(e.getKey());
+            }
+        }
+        return sb.toString();
+    }
+
+    // Solution2: HashMap + Bucket Sort
+    public String frequencySort1(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c: s.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        List<Character>[] bucket = new List[s.length() + 1];
+        for (char key : map.keySet()) {
+            int frequency = map.get(key);
+            if (bucket[frequency] == null) {
+                bucket[frequency] = new ArrayList<>();
+            }
+            bucket[frequency].add(key);
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int f = bucket.length - 1; f >= 0; --f) {
+            if (bucket[f] != null) {
+                for (char c : bucket[f]) {
+                    for (int j = 0; j < f; ++j) {
+                        sb.append(c);
+                    }
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
