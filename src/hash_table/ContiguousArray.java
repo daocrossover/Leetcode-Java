abd014a5bcccf3ab70626cbb74e231e111a307a1
daocrossover <475970883@qq.com>
2019-02-08 13:48:24 -0800

    2.8

---
+++ src/hash_table/ContiguousArray.java
@@ -0,0 +1,42 @@
+package hash_table;
+
+/* 525. Contiguous Array
+Description:
+Given a binary array, find the maximum length of a contiguous subarray with equal number of 0 and 1.
+
+Example 1:
+Input: [0,1]
+Output: 2
+Explanation: [0, 1] is the longest contiguous subarray with equal number of 0 and 1.
+
+Example 2:
+Input: [0,1,0]
+Output: 2
+Explanation: [0, 1] (or [1, 0]) is a longest contiguous subarray with equal number of 0 and 1.
+
+Note: The length of the given binary array will not exceed 50,000.
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContiguousArray {
+    // Solution1: PreSum + HashMap
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0, res = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            // change 0 in the original array to -1
+            sum += 2 * nums[i] - 1;
+            // if find the sum[i, j] == 0
+            // then we know there are even number of -1 and 1 between index i and j.
+            if (map.containsKey(sum)) {
+                res = Math.max(res, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        return res;
+    }
+}
---
+++ src/hash_table/SubarraySumEqualsK.java
@@ -0,0 +1,60 @@
+package hash_table;
+
+/* 560. Subarray Sum Equals K
+Description:
+Given an array of integers and an integer k,
+you need to find the total number of continuous subarrays whose sum equals to k.
+
+Example 1:
+Input:nums = [1,1,1], k = 2
+Output: 2
+
+Note:
+1. The length of the array is in range [1, 20,000].
+2. The range of numbers in the array is [-1000, 1000]
+and the range of the integer k is [-1e7, 1e7].
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySumEqualsK {
+    // Solution1: PreSum + HashMap
+    // if sum[i] - sum[j] = k, the sum of elements lying between indices i and j is k.
+    // Time Complexity: O(n), Space Complexity: O(n)
+    public int subarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int res = 0, sum = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                res += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+
+    // Solution2: Brute Force
+    // Time Complexity: O(n^2), Space Complexity: O(1)
+    public int subarraySum1(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int res = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            int sum = 0;
+            for (int j = i; j < nums.length; ++j) {
+                sum += nums[j];
+                if (sum == k) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
