2cbbfad1f4f6a421e865526c50b4921a2450652e
daocrossover <475970883@qq.com>
2019-02-26 09:27:12 -0800

    2.26

--- src/dynamic_programming/UniquePaths.java
+++ src/dynamic_programming/UniquePaths.java
@@ -23,6 +23,8 @@ Input: m = 7, n = 3
 Output: 28
  */
 
+import java.util.Arrays;
+
 public class UniquePaths {
     public int uniquePaths(int m, int n) {
         int M = m + n - 2;
@@ -33,5 +35,35 @@ public class UniquePaths {
             res /= i;
         }
         return (int)res;
+    }
+
+    // Solution2: Dynamic Programming
+    // When n = 0 or m = 0 the function always returns 1 since the robot can't go left or up.
+    // For all other cells. The result = uniquePaths(m-1, n) + uniquePaths(m, n-1)
+    int uniquePaths1(int m, int n) {
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                }
+            }
+        }
+        return dp[n-1][m-1];
+    }
+
+    // save spaces, Space Complexity: O(min(m, n))
+    public int uniquePaths2(int m, int n) {
+        if (m > n) return uniquePaths(n, m);
+        int[] dp = new int[m];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; ++i) {
+            for (int j = 1; j < m; ++j) {
+                dp[j] += dp[j-1];
+            }
+        }
+        return dp[m-1];
     }
 }
